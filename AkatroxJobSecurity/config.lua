a5576018559d85a5c8ad7f32b4bf73db384e934b
Akatrox <129941630+benakatrox@users.noreply.github.com>
2024-11-12 12:16:00 +0300

    Update config.lua

--- AkatroxJobSecurity/config.lua
+++ AkatroxJobSecurity/config.lua
@@ -1,8 +1,10 @@
 Config = {}
 
 -- Mesleklere göre silah izni ayarlama aşağıda eklediğiniz silahı yanında belirttiğiniz meslekler dışında kimse kullanamayacaktır.
+-- ["SilahAdı"] = {"Meslek1","Meslek2"},
 
--- ["Silah Adı"] = {"Meslek1","Meslek2"},
+-- Arranging gun permits according to professions. No one will be able to use the gun you added below except for the professions you specified.
+-- ["WeaponName"] = {"Job1","Job2"},
 
 Config.RestrictedItems = {
     weapons = {
@@ -16,8 +18,10 @@ Config.RestrictedItems = {
 }
 
 -- Mesleklere göre araç izni ayarlama aşağıda eklediğiniz aracı yanında belirttiğiniz meslekler dışında kimse kullanamayacaktır. (şoför hariç diğer koltuklara oturabilirler),
-
 -- ["Meslek Adı"] = {"Araç1","Araç2"},
+
+-- Setting vehicle permits according to professions. No one will be able to use the vehicle you added below except for the professions you specified. (they can sit in other seats except the driver),
+-- ["JobName"] = {"Vehicle1","Vehicle2"},
 
 Config.RestrictedVehicles = {
     ["police"] = {"police", "police2", "police3"},
