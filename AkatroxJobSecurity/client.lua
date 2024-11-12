1b07ad099b1f83e03b3e048f6cba9bd4589817d7
Akatrox <129941630+benakatrox@users.noreply.github.com>
2024-11-12 12:16:35 +0300

    Update client.lua

--- AkatroxJobSecurity/client.lua
+++ AkatroxJobSecurity/client.lua
@@ -1,6 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
--- Araç kullanım sınırlaması
 CreateThread(function()
     while true do
         local player = PlayerPedId()
@@ -12,14 +11,12 @@ CreateThread(function()
             local PlayerData = QBCore.Functions.GetPlayerData()
             local jobName = PlayerData.job and PlayerData.job.name
 
-            -- Eğer oyuncunun bir işi yoksa veya tanımlanmamışsa, sınırlı araçları sürmesine izin vermeyin
             if jobName and Config.RestrictedVehicles[jobName] then
                 if not has_value(Config.RestrictedVehicles[jobName], vehName) then
                     QBCore.Functions.Notify("Bu aracı sürme yetkiniz yok.", "error")
                     TaskLeaveVehicle(player, veh, 16) -- Oyuncuyu araçtan çıkar
                 end
             elseif Config.RestrictedVehicles["police"] and has_value(Config.RestrictedVehicles["police"], vehName) then
-                -- Eğer meslek "police" değilse, polisi sınırlı araçları kullanamasın
                 QBCore.Functions.Notify("Bu aracı sürme yetkiniz yok.", "error")
                 TaskLeaveVehicle(player, veh, 16)
             end
@@ -28,7 +25,6 @@ CreateThread(function()
     end
 end)
 
--- Silah kullanım sınırlaması
 CreateThread(function()
     while true do
         local player = PlayerPedId()
@@ -36,22 +32,18 @@ CreateThread(function()
         local PlayerData = QBCore.Functions.GetPlayerData()
         local jobName = PlayerData.job and PlayerData.job.name
 
-        -- Sınırlı bir silah mı kontrol edin
         for restrictedWeapon, allowedJobs in pairs(Config.RestrictedItems.weapons) do
             if GetHashKey(restrictedWeapon) == weapon then
-                -- Oyuncunun mesleği, bu silahı çekmeye yetkili mi kontrol et
                 if jobName and not has_job(allowedJobs, jobName) then
-                    -- Yetkisi yoksa, silahı kaldır ve uyarı ver
                     RemoveWeaponFromPed(player, weapon)
                     QBCore.Functions.Notify("Bu silahı çekme yetkiniz yok.", "error")
                 end
             end
         end
-        Wait(500) -- Her 0.5 saniyede bir kontrol et
+        Wait(500)
     end
 end)
 
--- Meslek doğrulama fonksiyonu
 function has_job(jobList, job)
     for _, v in ipairs(jobList) do
         if v == job then
@@ -61,7 +53,6 @@ function has_job(jobList, job)
     return false
 end
 
--- Bir değerin tabloda olup olmadığını kontrol eden yardımcı fonksiyon
 function has_value(tab, val)
     for _, value in ipairs(tab) do
         if value == val then
